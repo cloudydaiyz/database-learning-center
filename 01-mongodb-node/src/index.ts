670d087d038745d44f7fb72f09259a65df586a10
cloudydaiyz <kyland03.biz@gmail.com>
2024-07-27 10:16:07 -0500

    mongodb

---
+++ 01-mongodb-node/src/index.ts
@@ -0,0 +1,27 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { mongo_user, mongo_pass } from './secrets';
+const uri = `mongodb+srv://${mongo_user}:${mongo_pass}@cluster0.kbko7qq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
---
+++ 02-mongodb-node-lambda/ZIP.md
@@ -0,0 +1,1 @@
+`zip zip/output.zip -r dist`
